6e4d047931885379811f68840576d4153a472282
Adylan Roaffa <adylanrff@gmail.com>
2019-02-21 23:45:36 +0700

    add column for account get

--- payment/payment_usecase.py
+++ payment/payment_usecase.py
@@ -9,7 +9,7 @@ class PaymentUsecase:
     accounts = []
     for result in result_proxy:
       if (result):
-        accounts.append(Account(result[0],result[1],result[2]))
+        accounts.append(Account(result[0], result[1], result[2], result[3], result[4], result[5]))
 
     return accounts
     
