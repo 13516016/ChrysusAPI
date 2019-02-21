74328e594ab65aca11af9f66beab6d654fccf90c
Adylan Roaffa <adylanrff@gmail.com>
2019-02-21 18:21:33 +0700

    modified main.py

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ from news import news_model, news_repository, news_usecase, news_route
 import configparser
 
 app = Flask(__name__)
+app.register_blueprint(payment_route.payment_blueprint(usecases["payment"]), url_prefix='/payment')
 
 def read_config(config_filename='config.ini'):
   config = configparser.ConfigParser()
   config.read(config_filename)
   return config
-
-def create_app(usecases):
-  app.register_blueprint(payment_route.payment_blueprint(usecases["payment"]), url_prefix='/payment')
-  return app
 
 def connect_db(username, password, host, dbname ):
   engine = create_engine(f'postgresql://{username}:{password}@{host}/{dbname}')
@@ -33,18 +30,16 @@ def create_usecases(repositories):
   usecases["payment"]=payment_usecase.PaymentUsecase(repositories["payment"])
   return usecases
 
-if __name__ == "__main__":
-  config = read_config('config.ini')
-  engine = connect_db(
-    config["POSTGRESQL"]["Username"],
-    config["POSTGRESQL"]["Password"],
-    config["POSTGRESQL"]["Host"],
-    config["POSTGRESQL"]["DBName"])
-  create_models(engine)
-  db_connection = engine.connect()
-  repositories = create_repositories(db_connection)
-  usecases = create_usecases(repositories)
+config = read_config('config.ini')
+engine = connect_db(
+  config["POSTGRESQL"]["Username"],
+  config["POSTGRESQL"]["Password"],
+  config["POSTGRESQL"]["Host"],
+  config["POSTGRESQL"]["DBName"])
+create_models(engine)
+db_connection = engine.connect()
+repositories = create_repositories(db_connection)
+usecases = create_usecases(repositories)
 
-  app = create_app(usecases)
-  app.run()
-  
+
+app.run()
